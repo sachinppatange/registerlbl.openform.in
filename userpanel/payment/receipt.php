6c60643911bc31ebcf2c7691e24f6eec2b9d8c5f
sachinppatange <info@spdesign.in>
2025-11-10 01:52:08 +0530

    Initial commit: project scaffold

--- userpanel/payment/receipt.php
+++ userpanel/payment/receipt.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '/../config/wa_config.php';
-require_once __DIR__ . '/../auth.php';
+require_once __DIR__ . '/config/wa_config.php';
+require_once __DIR__ . '/auth.php';
 
 require_auth();
 $user = current_user();

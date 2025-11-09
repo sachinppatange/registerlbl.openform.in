ce7d3b86c510a2090ffeb60934a7eb2d3588b88e
sachinppatange <info@spdesign.in>
2025-11-10 00:51:14 +0530

    Initial commit: project scaffold

--- config/payment_config.php
+++ config/payment_config.php
@@ -4,7 +4,6 @@
 
 define('RZP_KEY_ID', getenv('rzp_live_D53J9UWwYtGimn') ?: '');
 define('RZP_KEY_SECRET', getenv('w0SnqzH2SOOIc0gnUR7cYO3r') ?: '');
-define('RZP_WEBHOOK_SECRET', getenv('9f2b3a1e8c4f7a6b0d2e5f8a1f2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0') ?: '');
 
 define('RAZORPAY_CURRENCY', getenv('INR') ?: 'INR');
 // Default amount in rupees (integer or float). Default Rs.1 as requested.

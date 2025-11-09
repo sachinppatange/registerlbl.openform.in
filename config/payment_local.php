8500a8808b34e0e42115f88396f58656aff86ce9
sachinppatange <info@spdesign.in>
2025-11-10 01:13:40 +0530

    Initial commit: project scaffold

--- config/payment_local.php
+++ config/payment_local.php
@@ -9,5 +9,5 @@ if (!defined('RZP_KEY_SECRET')) {
     define('RZP_KEY_SECRET', 'w0SnqzH2SOOIc0gnUR7cYO3r');
 }
 if (!defined('RZP_WEBHOOK_SECRET')) {
-    define('RZP_WEBHOOK_SECRET', 'your_webhook_secret_here');
+    define('RZP_WEBHOOK_SECRET', '9f2b3a1e8c4f7a6b0d2e5f8a1f2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0');
 }
